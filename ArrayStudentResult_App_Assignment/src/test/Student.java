1509f9790d432db2a31d997426c9dc687daefa64
krishna <krishna@LAPTOP-9DJ9AQ81.mshome.net>
2023-10-22 20:54:35 +0530

    Array of Students Results

---
+++ ArrayStudentResult_App_Assignment/src/test/Student.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Student {
+	public String name, rollNo, branch, result;
+	public int TotMarks;
+	public float per;
+
+	public String toString() {
+		return name + "\t" + rollNo + "\t" + branch + "\t" + result + "\t" + TotMarks + "\t" + per;
+	}
+}//class
