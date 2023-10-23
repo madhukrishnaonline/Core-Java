67a3cf1d51b883e35b7819429385cb28d00fdf77
krishna <krishna@LAPTOP-9DJ9AQ81.mshome.net>
2023-10-23 14:07:17 +0530

    Exception Checked Sleep

---
+++ Exception_App4_Pre_Checked_Sleep/src/mk/DemoException.java
@@ -0,0 +1,10 @@
+package mk;
+
+public class DemoException {
+	public static void main(String[] args) throws InterruptedException {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("I Love You");
+			Thread.sleep(2000);
+		}
+	}//main
+}//class
